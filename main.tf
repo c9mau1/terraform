d9609586705f87ef6755e54981b06abfda3148b8
c9mau1 <36461185+c9mau1@users.noreply.github.com>
2018-08-31 09:16:17 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+
 provider "azurerm" {
+        subscription_id = "${var.subscription_id}"
+        client_id  = "${var.client_id}"
+        client_secret  = "${var.client_secret"
+        tenant_id  = "${var.tenant_id}"
 }
 ### リソースグループ
 resource "azurerm_resource_group" "rg" {
