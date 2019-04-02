dace09929488bc93fe98fc77ede8d7d98a3ad12a
c9mau1 <36461185+c9mau1@users.noreply.github.com>
2019-04-03 00:00:53 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,6 @@
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-variable "env" {}
-
-
 provider "azurerm" {
-        subscription_id = "${var.subscription_id}"
-        client_id  = "${var.client_id}"
-        client_secret  = "${var.client_secret}"
-        tenant_id  = "${var.tenant_id}"
 }
+
 ### リソースグループ
 resource "azurerm_resource_group" "rg" {
         name = "${local.res_group}"
@@ -19,9 +9,3 @@ resource "azurerm_resource_group" "rg" {
                 environment = "${var.env}"
         }
 }
-
-### WebApp
-
-
-### ストレージアカウント
-
