7dc236ec0f615aee51b7b9f7b9b8fc37147a4a77
c9mau1 <36461185+c9mau1@users.noreply.github.com>
2019-04-03 00:01:34 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,4 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
         name = "${local.res_group}"
         location = "${local.region}"
-        tags = {
-                environment = "${var.env}"
-        }
 }
